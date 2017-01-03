0839a05056b5320c178cca0a3da4a55e9baa712a
Silvestr Baibuz <silvestr-b@yandex.ru>
2017-01-03 22:19:18 +0300

    first commit

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function removeFs(tree, dir){
 
 function createFs(node, dir){
 	for(let prop in node){
-		if(typeof(node[prop]) === 'string'){
+		if(typeof node[prop] === 'string'){
 			fs.writeFileSync(path.join(dir, prop), node[prop])
 		} else 
-		if(typeof(node[prop] === 'object' && node[prop] !== null && !Array.isArray(node[prop]))){
+		if(typeof node[prop] === 'object' && node[prop] !== null && !Array.isArray(node[prop])){
 			if(!fs.existsSync(path.join(dir, prop))){
 				fs.mkdirSync(path.join(dir, prop))
 			}
--- test/index.js
+++ test/index.js
@@ -2,10 +2,72 @@
 
 const fs = require('fs-extra');
 const path = require('path');
-const expect = require('mocha').expect;
+const expect = require('chai').expect;
 const builder = require('../index');
+const tmplFolder = path.join(__dirname, 'tmpl')
 
 
 describe('fs-builder', () => {
-	
+
+	describe('createFs', () => {
+
+		beforeEach(() => {
+			fs.emptyDirSync(tmplFolder)
+		})
+
+		it('Должен создавать пустую директорию если значение пустой объект', () => {
+			builder.createFs({root: {}}, tmplFolder)
+
+			expect(fs.existsSync(path.join(tmplFolder, 'root'))).to.be.true
+		})
+
+		it('Если значение строка то должен создавать файл с именем равным имени свойства и содержанием равным значению', () => {
+			builder.createFs({'root.js': 'value'}, tmplFolder);
+
+			expect(fs.existsSync(path.join(tmplFolder, 'root.js'))).to.be.true;
+			expect(fs.readFileSync(path.join(tmplFolder, 'root.js'), 'utf-8')).to.be.equal('value');
+		})
+
+		it('Если значение - объект, должен создать директорию с именем равным свойству и содержимым указанным в объекте', () => {
+			builder.createFs({root: { 'file.js': 'value' }}, tmplFolder);
+
+			expect(fs.existsSync(path.join(tmplFolder, 'root'))).to.be.true;
+			expect(fs.existsSync(path.join(tmplFolder, 'root', 'file.js'))).to.be.true;
+			expect(fs.readFileSync(path.join(tmplFolder, 'root', 'file.js'), 'utf-8')).to.be.equal('value');
+		})
+		
+		it('Если несколько объектов вложенные, то создает вложенную структуру папок', () => {
+			builder.createFs({ dir1: { dir2: { dir3 : {} } } }, tmplFolder);
+
+			expect(fs.existsSync(path.join(tmplFolder, 'dir1'))).to.be.true;
+			expect(fs.existsSync(path.join(tmplFolder, 'dir1', 'dir2'))).to.be.true;
+			expect(fs.existsSync(path.join(tmplFolder, 'dir1', 'dir2', 'dir3'))).to.be.true;
+		})
+
+		it('Если значение не объект и не строка должен бросить исключение', () => {
+			[5, [], null, () => {}].forEach(val => {
+				expect(() => { builder.createFs({ root: val }, tmplFolder) }).to.throw(Error, /prop in tree must be an object or a string/);
+			})
+		})
+
+	})
+
+	describe('removeFs', () => {
+		beforeEach(() => {
+			fs.emptyDirSync(tmplFolder);
+			builder.createFs({ dir1: { dir2: { dir3 : { 'file.js': ''} } } }, tmplFolder);
+		})
+
+		it('Должен удалять все переданное дерево со всем содержимым', () => {
+			builder.removeFs({ dir1: { dir2: { dir3 : { 'file.js': ''} } } }, tmplFolder);
+		})
+
+		it('Должен удалять указанную директорию со всем содержимым', () => {
+			builder.removeFs({ dir1: {} }, tmplFolder);
+		})
+	})
+
+	after(() => {
+		fs.removeSync(tmplFolder)
+	})
 })
